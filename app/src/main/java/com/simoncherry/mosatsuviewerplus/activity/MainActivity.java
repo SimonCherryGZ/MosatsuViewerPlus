a53a7283a4d91e3fbb6bbf6d67b1c7641632fe31
SimonCherry <simoncherry@sina.com>
2017-04-09 20:34:02 +0800

    1.修改应用Icon。2.修改启动页UI。

--- app/src/main/java/com/simoncherry/mosatsuviewerplus/activity/MainActivity.java
+++ app/src/main/java/com/simoncherry/mosatsuviewerplus/activity/MainActivity.java
@@ -1,28 +1,54 @@
 package com.simoncherry.mosatsuviewerplus.activity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.simoncherry.mosatsuviewerplus.R;
-import com.simoncherry.mosatsuviewerplus.fragment.HorizontalPagerFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Context mContext;
+    @BindView(R.id.layout_mosatsu)
+    RelativeLayout layoutMosatsu;
+    @BindView(R.id.layout_gallery)
+    RelativeLayout layoutGallery;
+
+    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = MainActivity.this;
+        unbinder = ButterKnife.bind(this);
+    }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        HorizontalPagerFragment horizontalPagerFragment = new HorizontalPagerFragment();
-        fragmentTransaction.add(R.id.layout_root, horizontalPagerFragment);
-        fragmentTransaction.commit();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    private void startActivity(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+    }
+
+    @OnClick({R.id.layout_mosatsu, R.id.layout_gallery})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_mosatsu:
+                startActivity(MosatsuActivity.class);
+                break;
+            case R.id.layout_gallery:
+                startActivity(GalleryActivity.class);
+                break;
+        }
     }
 }
